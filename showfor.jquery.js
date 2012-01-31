e88f67990d245a29b2cd0f924b949826756076f8
Matt Zandstra <matthew@avenirit.com>
2012-01-31 10:02:29 -0600

    added support for IE, Opera, and other browsers that dont support display:none on <option>

--- showfor.jquery.js
+++ showfor.jquery.js
@@ -4,23 +4,31 @@ $.fn.showfor = function(parent, hideChild, useDisabled) {
     if(typeof(hideChild)=="undefined") hideChild = false;
     if(typeof(useDisabled)=="undefined") useDisabled = true;
     
-    if(hideChild) $(this).find("option[value!='']").hide();
+    // Clone the child to keep a list of all the elements.
+    childClone = $(this).clone();
     
-    // disable child select if option is set.
+    // If the hide child config is set remove the child options until a parent is chosen.
+    if(hideChild) $(this).find("option[value!='']").remove();
+    
+    // Disable the child select if the config is specified.
     if(useDisabled) $(this).attr('disabled', true);
     
-    // add handler for parent.
-    $(parent).change((function(child, useDisabled) {
+    /* Add a handler for parent.
+     *   child is the dependent select.
+     *   useDisabled is the option.
+     *   originalClone is a clone of the child element with all the options enabled.
+     */
+    $(parent).change((function(child, useDisabled, originalClone) {
         return function() {
             
             // enable child if using disabled
             if(useDisabled) $(child).removeAttr('disabled');
             
             // hide all child options
-            $(child).find("option[value!='']").hide();
+            $(child).find("option[value!='']").remove();
             
             // show available child options
-            $(child).find("option[value!=''].show-for-"+this.value).show();
+            $(originalClone).find("option[value!=''].show-for-"+this.value).appendTo(child);
             
             // set child to blank option.
             $(child).val("");
@@ -31,6 +39,6 @@ $.fn.showfor = function(parent, hideChild, useDisabled) {
             // disable element if parent is blank and using disabled.
             if(useDisabled && this.value == "") $(child).attr("disabled", true);
         }
-    })(this, useDisabled));
+    })(this, useDisabled, childClone));
     
 }
